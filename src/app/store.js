ccbd32d07b0fbb1e4430648aa1ca7e9081688bd7
Salekin Imran <abir045@gmail.com>
2023-01-16 14:02:06 +0600

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counterSlice/counterSlice";
+// import counterReducer from "../features/counterSlice/counterSlice";
+import postsReducer from "../features/post/postSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    posts: postsReducer,
   },
 });
